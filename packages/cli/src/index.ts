69042059e2f8b89043572f30a5e0917b9a16078f
underthestars-zhy <zhuhaoyu0909@icloud.com>
2025-08-07 00:49:15 +0800

    Add cancellation check and prompt for hackathon name
    Introduce a `checkCancellation` utility to handle user cancellations and integrate it into the CLI flow. Add a prompt for entering the hackathon name using `@clack/prompts` and update `package.json` with necessary TypeScript typings for Node.js.

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,8 +1,16 @@
-import { intro, outro } from '@clack/prompts';
+import {intro, outro, text} from '@clack/prompts';
 import pc from "picocolors"
+import {checkCancellation} from "./check-cancellation";
 
 async function main(): Promise<void> {
     intro(pc.inverse('setup-united-portal'));
+
+    const hackathonName = await text({
+        message: "What's the name of your hackathon?",
+        placeholder: "MyHackathon"
+    })
+
+    checkCancellation(hackathonName)
 
     outro(pc.green('You\'re all set!'));
 }
