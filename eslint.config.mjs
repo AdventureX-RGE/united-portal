cd7880636328fa36470948138f4046d4631b0945
Marco <marco.shengqi@gmail.com>
2025-08-07 23:29:51 +0800

    add: update ESLint configuration and dependencies for React and TypeScript support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,6 @@
-import { dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
 import antfu from '@antfu/eslint-config'
-import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
+export default antfu({
+    react: true,
+    typescript: true,
 })
-
-const eslintConfig = [
-  {
-    ignores: ['**/*', '!src/**'],
-  },
-  ...compat.extends('next', 'next/core-web-vitals', 'next/typescript'),
-]
-
-export default antfu(eslintConfig)
