30369584150fabea281a2e82ddaee5eed89523db
Marco <marco.shengqi@gmail.com>
2025-08-07 23:20:58 +0800

    refactor: standardize quotes and formatting across configuration and source files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-const db = drizzle(process.env.DATABASE_URL!);
+import { drizzle } from 'drizzle-orm/node-postgres'
+import 'dotenv/config'
+
+// eslint-disable-next-line unused-imports/no-unused-vars,node/prefer-global/process
+const db = drizzle(process.env.DATABASE_URL!)
